a792df466cdca521ac30193f145517f14f457933
Jaden Locke <jadenlocke1113@gmail.com>
2020-07-21 18:12:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -31,7 +31,7 @@ def game_hash
            :steals => 4,
            :blocks => 5,
            :slam_dunks => 5 },
-         { :player_name => "Ben Gordan",
+         { :player_name => "Ben Gordon",
            :number => 8,
            :shoe => 15,
            :points => 33,
